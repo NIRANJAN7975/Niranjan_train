254039fd5693d0b923e99648b26f8d9d49b915bc
NIRANJAN7975 <116199094+NIRANJAN7975@users.noreply.github.com>
2025-11-19 21:44:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -439,7 +439,7 @@ def send_otp(email):
 
 
     sender_email = "railconnect24.7@gmail.com"
-    sender_password = "cxvq xiyp jxqj xzhi"
+    sender_password = "zpkv pnni oksk wwfp"
 
 
     # Create the email message
@@ -840,4 +840,5 @@ def App_review():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
